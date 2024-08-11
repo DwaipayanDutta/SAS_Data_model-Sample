ef8abf49b817505e0980ad37ec81cc3ed3c17aab
Dwaipayan <dwaipayandutt@gmail.com>
2024-08-11 14:39:24 +0530

    Update Master_File.py

--- Master_File.py
+++ Master_File.py
@@ -36,6 +36,7 @@ def display_script_banner():
     """
     print(banner)
 
+
 class DataGenerator:
     def __init__(self, n_samples=10000, export_path='dataset.csv'):
         np.random.seed(42)
@@ -47,29 +48,31 @@ class DataGenerator:
 
     def generate_continuous_data(self):
         cont_dict = {
-            'Variable': ['AGE', 'AQB_BALANCE', 'AUM', 'CIBIL_SCORE', 'CREDIT_CARD_LIMIT', 'CR_AMT_12MNTH', 'CR_CNT_12MNTH',
-                         'DC_APPAREL_30DAYS_ACTV', 'DC_ECOM_30DAYS_AMT', 'DC_ECOM_30DAYS_CNT', 'DC_FOOD_30DAYS_ACTV',
-                         'DC_FUEL_30DAYS_ACTV', 'DC_GROCRY_30DAYS_ACTV', 'DC_OTT_30DAYS_ACTV', 'DC_POS_30DAYS_AMT',
-                         'DC_POS_30DAYS_CNT', 'DC_RECHARGE_30DAYS_ACTV', 'DC_TRAVEL_30DAYS_ACTV', 'DC_UTILITY_30DAYS_ACTV',
-                         'DR_AMT_12MNTH', 'DR_CNT_12MNTH', 'DR_CR_RATIO', 'FD_COUNT', 'FD_CURRENTMONTHANR', 'INCOME_NET',
-                         'KYC_LAST_DONE_DATE', 'MONTHLY_BALANCE', 'NRV', 'TOTAL_LIVE_SECURED_AMT', 'TOTAL_LIVE_UNSECURED_AMT',
-                         'VINTAGE_DAYS'],
-            'Mean': [45.75, 335324.5275, 1482679.66, 729.25, 432750.0, 4427092.8125, 390.0, 0.0, 747.54, 0.5, 0.0, 0.25, 0.25,
-                     0.0, 1250.0, 0.75, 0.0, 0.0, 0.0, 4400780.205, 755.25, 18.4664255, 2.25, 818797.965, 637578.845, 22694.0,
-                     285443.28500000003, 1427096.995, 8043387.25, 1626783.5, 2472.25],
-            'Std Dev': [13.2034277, 40797470.12, 31660895.68, 145.8168632, 227937.36, 160056333.0, 405.0004491, 0.0778686,
-                        55320.12, 1.1421999, 0.0979862, 0.1093538, 0.1182655, 0.081972, 6117.33, 1.1075376, 0.0678652, 0.0594367,
-                        0.0595447, 163060150.0, 435.6866233, 249.0156422, 1.8768659, 2177717.88, 272956.6, 786.2686886,
-                        39394146.0, 31877424.32, 24655099.6, 3300140.98, 1216.33]
+            'Variable': ['AGE', 'AQB_BALANCE', 'AUM', 'CIBIL_SCORE', 'CREDIT_CARD_LIMIT', 'CR_AMT_12MNTH', 
+                         'CR_CNT_12MNTH', 'DC_APPAREL_30DAYS_ACTV', 'DC_ECOM_30DAYS_AMT', 'DC_ECOM_30DAYS_CNT', 
+                         'DC_FOOD_30DAYS_ACTV', 'DC_FUEL_30DAYS_ACTV', 'DC_GROCRY_30DAYS_ACTV', 
+                         'DC_OTT_30DAYS_ACTV', 'DC_POS_30DAYS_AMT', 'DC_POS_30DAYS_CNT', 
+                         'DC_RECHARGE_30DAYS_ACTV', 'DC_TRAVEL_30DAYS_ACTV', 'DC_UTILITY_30DAYS_ACTV', 
+                         'DR_AMT_12MNTH', 'DR_CNT_12MNTH', 'DR_CR_RATIO', 'FD_COUNT', 
+                         'FD_CURRENTMONTHANR', 'INCOME_NET', 'KYC_LAST_DONE_DATE', 'MONTHLY_BALANCE', 
+                         'NRV', 'TOTAL_LIVE_SECURED_AMT', 'TOTAL_LIVE_UNSECURED_AMT', 'VINTAGE_DAYS'],
+            'Mean': [45.75, 335324.5275, 1482679.66, 729.25, 432750.0, 4427092.8125, 390.0, 0.0, 747.54, 0.5, 
+                     0.0, 0.25, 0.25, 0.0, 1250.0, 0.75, 0.0, 0.0, 0.0, 4400780.205, 755.25, 
+                     18.4664255, 2.25, 818797.965, 637578.845, 22694.0, 285443.28500000003, 
+                     1427096.995, 8043387.25, 1626783.5, 2472.25],
+            'Std Dev': [13.2034277, 40797470.12, 31660895.68, 145.8168632, 227937.36, 160056333.0, 405.0004491, 
+                        0.0778686, 55320.12, 1.1421999, 0.0979862, 0.1093538, 0.1182655, 0.081972, 
+                        6117.33, 1.1075376, 0.0678652, 0.0594367, 0.0595447, 163060150.0, 435.6866233, 
+                        249.0156422, 1.8768659, 2177717.88, 272956.6, 786.2686886, 39394146.0, 
+                        31877424.32, 24655099.6, 3300140.98, 1216.33]
         }
-
         data_summary = pd.DataFrame(cont_dict)
-        data_summary = data_summary[~data_summary['Variable'].isin(['FD_FLAG', 'GI_FLAG', 'HEALTH_FLAG', 'LI_FLAG', 'MASS_FLAG',
-                                                                   'MF_FLAG', 'NR_FLAG'])]
+        data_summary = data_summary[~data_summary['Variable'].isin(['FD_FLAG', 'GI_FLAG', 'HEALTH_FLAG', 
+                                                                    'LI_FLAG', 'MASS_FLAG', 'MF_FLAG', 'NR_FLAG'])]
 
         # Continuous data
         self.continuous_data = pd.concat([
-            pd.DataFrame({row['Variable']: np.random.gamma(row['Mean'], row['Std Dev'], size=self.n_samples)})
+            pd.DataFrame({row['Variable']: np.random.gamma(row['Mean'], row['Std Dev'], size=self.n_samples)}) 
             for _, row in data_summary.iterrows()
         ], axis=1)
 
@@ -96,11 +99,13 @@ class DataGenerator:
             'NOMINEE_AVAILABLE_FLAG': ['Y', 'N'],
             'RM_ALLOCATED_FLAG': ['Y', 'N'],
             'OCCUPATION': ['SELF EMPLOYED', 'SALARIED', 'BUSINESS OWNER', 'NON-EARNER'],
-            'STATE': ['Maharashtra', 'Gujarat', 'West Bengal', 'Uttar Pradesh', 'Tamil Nadu', 'Haryana', 'Rajasthan', 'Punjab',
-                      'Delhi', 'Madhya Pradesh', 'Out of India', 'Andhra Pradesh', 'Karnataka', 'Telangana', 'Odisha', 'Kerala',
-                      'Bihar', 'Assam', 'Chhattisgarh', 'Jharkhand', 'Uttarakhand', 'Himachal Pradesh', 'Jammu and Kashmir',
-                      'Chandigarh', 'Goa', 'Manipur', 'Nagaland', 'Tripura', 'Puducherry', 'Dadra and Nagar Haveli and Daman and Diu',
-                      'Sikkim', 'Arunachal Pradesh', 'Meghalaya', 'Mizoram', 'Andaman and Nicobar']
+            'STATE': ['Maharashtra', 'Gujarat', 'West Bengal', 'Uttar Pradesh', 'Tamil Nadu', 'Haryana', 
+                      'Rajasthan', 'Punjab', 'Delhi', 'Madhya Pradesh', 'Out of India', 'Andhra Pradesh', 
+                      'Karnataka', 'Telangana', 'Odisha', 'Kerala', 'Bihar', 'Assam', 'Chhattisgarh', 
+                      'Jharkhand', 'Uttarakhand', 'Himachal Pradesh', 'Jammu and Kashmir', 'Chandigarh', 
+                      'Goa', 'Manipur', 'Nagaland', 'Tripura', 'Puducherry', 
+                      'Dadra and Nagar Haveli and Daman and Diu', 'Sikkim', 'Arunachal Pradesh', 
+                      'Meghalaya', 'Mizoram', 'Andaman and Nicobar']
         }
 
         # Probabilities for categorical variables
@@ -114,22 +119,24 @@ class DataGenerator:
             'NOMINEE_AVAILABLE_FLAG': [0.95, 0.05],
             'RM_ALLOCATED_FLAG': [0.98, 0.02],
             'OCCUPATION': [0.404, 0.339, 0.152, 0.105],
-            'STATE': [0.119496018, 0.089888389, 0.07827775, 0.072670547, 0.064865747, 0.062475027, 0.050854399, 0.049988679,
-                      0.046685624, 0.043482459, 0.036553368, 0.033316907, 0.03284409, 0.031895128, 0.030473349, 0.029424496,
-                      0.028928372, 0.020664074, 0.018809435, 0.015855997, 0.007232093, 0.006562825, 0.005510642, 0.005430729,
-                      0.005330838, 0.00238739, 0.001664846, 0.001534988, 0.001481713, 0.001328547, 0.001288591, 0.001175382,
-                      0.00107882, 0.000526091, 1.66485E-05]
+            'STATE': [0.119496018, 0.089888389, 0.07827775, 0.072670547, 0.064865747, 0.062475027, 
+                      0.050854399, 0.049988679, 0.046685624, 0.043482459, 0.036553368, 0.033316907, 
+                      0.03284409, 0.031895128, 0.030473349, 0.029424496, 0.028928372, 0.020664074, 
+                      0.018809435, 0.015855997, 0.007232093, 0.006562825, 0.005510642, 0.005430729, 
+                      0.005330838, 0.00238739, 0.001664846, 0.001534988, 0.001481713, 0.001328547, 
+                      0.001288591, 0.001175382, 0.00107882, 0.000526091, 1.66485E-05]
         }
 
         # List of flag variables
         flag_variables = ['FD_FLAG', 'GI_FLAG', 'HEALTH_FLAG', 'LI_FLAG', 'MASS_FLAG', 'MF_FLAG', 'NR_FLAG']
-
         for flag_var in flag_variables:
             cat_variables[flag_var] = ['Y', 'N']
             prob[flag_var] = [0.5, 0.5]
 
         # Categorical data
-        self.categorical_data = pd.DataFrame({col: np.random.choice(cat_variables[col], self.n_samples, p=prob[col]) for col in cat_variables})
+        self.categorical_data = pd.DataFrame({
+            col: np.random.choice(cat_variables[col], self.n_samples, p=prob[col]) for col in cat_variables
+        })
         self.categorical_data['ACCOUNT_NO'] = self.categorical_data.index
 
     def combine_data(self):
@@ -181,10 +188,10 @@ class FeatureScaler:
         self.scaler = StandardScaler()
 
     def fit_transform(self, X_train):
-        return self.scaler.fit_transform(X_train)
+        return pd.DataFrame(self.scaler.fit_transform(X_train), columns=X_train.columns)
 
     def transform(self, X_test):
-        return self.scaler.transform(X_test)
+        return pd.DataFrame(self.scaler.transform(X_test), columns=X_test.columns)
 
 class ModelEvaluator:
     def __init__(self, classifiers):
@@ -231,7 +238,6 @@ class SHAPExplainer:
         else:
             raise ValueError("Unsupported model type for SHAP explainer")
         
-        
         try:
             self.shap_values = self.explainer.shap_values(X_test)
         except shap.ExplainerError as e:
@@ -243,6 +249,12 @@ class SHAPExplainer:
         plt.figure(figsize=(10, 6))
         shap.summary_plot(self.shap_values, self.X_test, plot_type="bar")
         plt.show()
+
+    def print_shap_values(self):
+        feature_names = self.X_test.columns
+        shap_values_df = pd.DataFrame(self.shap_values, columns=feature_names)
+        print("\nShapley Values:")
+        print(shap_values_df)
 
 # Finally Jay ram ji ki 
 if __name__ == "__main__":
@@ -281,3 +293,4 @@ if __name__ == "__main__":
     best_model = classifiers[best_classifier_name]
     explainer = SHAPExplainer(best_model, X_train_scaled, X_test_scaled)
     explainer.plot_shap_values()
+    explainer.print_shap_values()  # Print the Shapley values with feature names
