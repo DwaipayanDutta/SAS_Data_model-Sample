db6f33e7d8f96e5f1da4bebee5766d5430f0859f
Dwaipayan <dwaipayandutt@gmail.com>
2024-08-12 13:40:47 +0530

    updated

--- Master_File.py
+++ Master_File.py
@@ -19,6 +19,8 @@ from tqdm import tqdm
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
+required_packages = ['numpy', 'pandas', 'colorama']
+
 def display_script_banner():
     owner_name = 'By: Dwaipayan Dutta'
     banner = rf"""
@@ -36,7 +38,6 @@ def display_script_banner():
     """
     print(banner)
 
-
 class DataGenerator:
     def __init__(self, n_samples=10000, export_path='dataset.csv'):
         np.random.seed(42)
@@ -48,40 +49,57 @@ class DataGenerator:
 
     def generate_continuous_data(self):
         cont_dict = {
-            'Variable': ['AGE', 'AQB_BALANCE', 'AUM', 'CIBIL_SCORE', 'CREDIT_CARD_LIMIT', 'CR_AMT_12MNTH', 
-                         'CR_CNT_12MNTH', 'DC_APPAREL_30DAYS_ACTV', 'DC_ECOM_30DAYS_AMT', 'DC_ECOM_30DAYS_CNT', 
-                         'DC_FOOD_30DAYS_ACTV', 'DC_FUEL_30DAYS_ACTV', 'DC_GROCRY_30DAYS_ACTV', 
-                         'DC_OTT_30DAYS_ACTV', 'DC_POS_30DAYS_AMT', 'DC_POS_30DAYS_CNT', 
-                         'DC_RECHARGE_30DAYS_ACTV', 'DC_TRAVEL_30DAYS_ACTV', 'DC_UTILITY_30DAYS_ACTV', 
-                         'DR_AMT_12MNTH', 'DR_CNT_12MNTH', 'DR_CR_RATIO', 'FD_COUNT', 
-                         'FD_CURRENTMONTHANR', 'INCOME_NET', 'KYC_LAST_DONE_DATE', 'MONTHLY_BALANCE', 
-                         'NRV', 'TOTAL_LIVE_SECURED_AMT', 'TOTAL_LIVE_UNSECURED_AMT', 'VINTAGE_DAYS'],
-            'Mean': [45.75, 335324.5275, 1482679.66, 729.25, 432750.0, 4427092.8125, 390.0, 0.0, 747.54, 0.5, 
-                     0.0, 0.25, 0.25, 0.0, 1250.0, 0.75, 0.0, 0.0, 0.0, 4400780.205, 755.25, 
-                     18.4664255, 2.25, 818797.965, 637578.845, 22694.0, 285443.28500000003, 
-                     1427096.995, 8043387.25, 1626783.5, 2472.25],
-            'Std Dev': [13.2034277, 40797470.12, 31660895.68, 145.8168632, 227937.36, 160056333.0, 405.0004491, 
-                        0.0778686, 55320.12, 1.1421999, 0.0979862, 0.1093538, 0.1182655, 0.081972, 
-                        6117.33, 1.1075376, 0.0678652, 0.0594367, 0.0595447, 163060150.0, 435.6866233, 
-                        249.0156422, 1.8768659, 2177717.88, 272956.6, 786.2686886, 39394146.0, 
-                        31877424.32, 24655099.6, 3300140.98, 1216.33]
+            'Variable': ['AGE', 'AQB_BALANCE', 'AUM', 'CIBIL_SCORE', 'CREDIT_CARD_LIMIT', 'CR_AMT_12MNTH', 'CR_CNT_12MNTH',
+                         'DC_APPAREL_30DAYS_ACTV', 'DC_ECOM_30DAYS_AMT', 'DC_ECOM_30DAYS_CNT', 'DC_FOOD_30DAYS_ACTV',
+                         'DC_FUEL_30DAYS_ACTV', 'DC_GROCRY_30DAYS_ACTV', 'DC_OTT_30DAYS_ACTV', 'DC_POS_30DAYS_AMT',
+                         'DC_POS_30DAYS_CNT', 'DC_RECHARGE_30DAYS_ACTV', 'DC_TRAVEL_30DAYS_ACTV', 'DC_UTILITY_30DAYS_ACTV',
+                         'DR_AMT_12MNTH', 'DR_CNT_12MNTH', 'DR_CR_RATIO', 'FD_COUNT', 'FD_CURRENTMONTHANR', 'INCOME_NET',
+                         'KYC_LAST_DONE_DATE', 'MONTHLY_BALANCE', 'NRV', 'TOTAL_LIVE_SECURED_AMT', 'TOTAL_LIVE_UNSECURED_AMT',
+                         'VINTAGE_DAYS'],
+            'Mean': [45.75, 335324.5275, 1482679.66, 729.25, 432750.0, 4427092.8125, 390.0, 0.0, 747.54, 0.5, 0.0, 0.25, 0.25,
+                     0.0, 1250.0, 0.75, 0.0, 0.0, 0.0, 4400780.205, 755.25, 18.4664255, 2.25, 818797.965, 637578.845, 22694.0,
+                     285443.28500000003, 1427096.995, 8043387.25, 1626783.5, 2472.25],
+            'Std Dev': [13.2034277, 40797470.12, 31660895.68, 145.8168632, 227937.36, 160056333.0, 405.0004491, 0.0778686,
+                        55320.12, 1.1421999, 0.0979862, 0.1093538, 0.1182655, 0.081972, 6117.33, 1.1075376, 0.0678652, 0.0594367,
+                        0.0595447, 163060150.0, 435.6866233, 249.0156422, 1.8768659, 2177717.88, 272956.6, 786.2686886,
+                        39394146.0, 31877424.32, 24655099.6, 3300140.98, 1216.33]
         }
+
         data_summary = pd.DataFrame(cont_dict)
-        data_summary = data_summary[~data_summary['Variable'].isin(['FD_FLAG', 'GI_FLAG', 'HEALTH_FLAG', 
-                                                                    'LI_FLAG', 'MASS_FLAG', 'MF_FLAG', 'NR_FLAG'])]
 
-        # Continuous data
-        self.continuous_data = pd.concat([
-            pd.DataFrame({row['Variable']: np.random.gamma(row['Mean'], row['Std Dev'], size=self.n_samples)}) 
-            for _, row in data_summary.iterrows()
-        ], axis=1)
-
-        # Apply constraints
-        self.continuous_data['AGE'] = np.random.randint(1, 100, size=self.n_samples)  # AGE between 1 and 99
-        self.continuous_data['AQB_BALANCE'] = np.random.uniform(0, 10000000, size=self.n_samples)  # AQB_BALANCE between 0 and 10,000,000
-        self.continuous_data['CIBIL_SCORE'] = np.random.randint(300, 901, size=self.n_samples)  # CIBIL_SCORE between 300 and 900
-        self.continuous_data['CREDIT_CARD_LIMIT'] = np.random.uniform(0, 1000000, size=self.n_samples)  # CREDIT_CARD_LIMIT between 0 and 1,000,000
-        self.continuous_data['VINTAGE_DAYS'] = np.random.randint(1, 10001, size=self.n_samples)  # VINTAGE_DAYS between 1 and 10,000
+        # Continuous data generation with constraints
+        self.continuous_data = pd.DataFrame({
+            'AGE': np.random.randint(1, 100, size=self.n_samples),  # AGE between 1 and 99
+            'AQB_BALANCE': np.random.uniform(0, 10_000_000, size=self.n_samples),  # AQB_BALANCE between 0 and 10,000,000
+            'AUM': np.random.randint(0, 10_000_000, size=self.n_samples),  # AUM not more than 10,000,000
+            'CIBIL_SCORE': np.random.randint(300, 901, size=self.n_samples),  # CIBIL_SCORE between 300 and 900
+            'CREDIT_CARD_LIMIT': np.random.randint(0, 50_000_000, size=self.n_samples),  # CREDIT_CARD_LIMIT not more than 50,000,000
+            'CR_AMT_12MNTH': np.random.randint(0, 50_000_000, size=self.n_samples),  # CR_AMT_12MNTH not more than 50,000,000
+            'CR_CNT_12MNTH': np.random.randint(0, 21, size=self.n_samples),  # CR_CNT_12MNTH not more than 20
+            'DC_APPAREL_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_ECOM_30DAYS_AMT': np.random.randint(0, 1_500_000, size=self.n_samples),  # not more than 1,500,000
+            'DC_ECOM_30DAYS_CNT': np.random.randint(0, 501, size=self.n_samples),  # not more than 500
+            'DC_FOOD_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_FUEL_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_GROCRY_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_OTT_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_POS_30DAYS_AMT': np.random.randint(0, 4_000_000, size=self.n_samples),  # not more than 4,000,000
+            'DC_POS_30DAYS_CNT': np.random.randint(0, 101, size=self.n_samples),  # not more than 100
+            'DC_RECHARGE_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_TRAVEL_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DC_UTILITY_30DAYS_ACTV': np.random.randint(0, 11, size=self.n_samples),  # not more than 10
+            'DR_AMT_12MNTH': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'DR_CNT_12MNTH': np.random.randint(0, 21, size=self.n_samples),  # not more than 20
+            'DR_CR_RATIO': np.random.uniform(0, 1, size=self.n_samples),  # DR_CR_RATIO between 0 and 1
+            'FD_COUNT': np.random.randint(0, 21, size=self.n_samples),  # FD_COUNT not more than 20
+            'FD_CURRENTMONTHANR': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'INCOME_NET': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'MONTHLY_BALANCE': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'NRV': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'TOTAL_LIVE_SECURED_AMT': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'TOTAL_LIVE_UNSECURED_AMT': np.random.uniform(0, 10_000_000, size=self.n_samples),  # not more than 10,000,000
+            'VINTAGE_DAYS': np.random.randint(0, 366, size=self.n_samples)  # VINTAGE_DAYS not more than 365
+        })
 
         # Generate KYC_LAST_DONE_DATE between 01-01-2020 and 01-12-2023
         start_date = np.datetime64('2020-01-01')
@@ -99,13 +117,11 @@ class DataGenerator:
             'NOMINEE_AVAILABLE_FLAG': ['Y', 'N'],
             'RM_ALLOCATED_FLAG': ['Y', 'N'],
             'OCCUPATION': ['SELF EMPLOYED', 'SALARIED', 'BUSINESS OWNER', 'NON-EARNER'],
-            'STATE': ['Maharashtra', 'Gujarat', 'West Bengal', 'Uttar Pradesh', 'Tamil Nadu', 'Haryana', 
-                      'Rajasthan', 'Punjab', 'Delhi', 'Madhya Pradesh', 'Out of India', 'Andhra Pradesh', 
-                      'Karnataka', 'Telangana', 'Odisha', 'Kerala', 'Bihar', 'Assam', 'Chhattisgarh', 
-                      'Jharkhand', 'Uttarakhand', 'Himachal Pradesh', 'Jammu and Kashmir', 'Chandigarh', 
-                      'Goa', 'Manipur', 'Nagaland', 'Tripura', 'Puducherry', 
-                      'Dadra and Nagar Haveli and Daman and Diu', 'Sikkim', 'Arunachal Pradesh', 
-                      'Meghalaya', 'Mizoram', 'Andaman and Nicobar']
+            'STATE': ['Maharashtra', 'Gujarat', 'West Bengal', 'Uttar Pradesh', 'Tamil Nadu', 'Haryana', 'Rajasthan', 'Punjab',
+                      'Delhi', 'Madhya Pradesh', 'Out of India', 'Andhra Pradesh', 'Karnataka', 'Telangana', 'Odisha', 'Kerala',
+                      'Bihar', 'Assam', 'Chhattisgarh', 'Jharkhand', 'Uttarakhand', 'Himachal Pradesh', 'Jammu and Kashmir',
+                      'Chandigarh', 'Goa', 'Manipur', 'Nagaland', 'Tripura', 'Puducherry', 'Dadra and Nagar Haveli and Daman and Diu',
+                      'Sikkim', 'Arunachal Pradesh', 'Meghalaya', 'Mizoram', 'Andaman and Nicobar']
         }
 
         # Probabilities for categorical variables
@@ -119,12 +135,11 @@ class DataGenerator:
             'NOMINEE_AVAILABLE_FLAG': [0.95, 0.05],
             'RM_ALLOCATED_FLAG': [0.98, 0.02],
             'OCCUPATION': [0.404, 0.339, 0.152, 0.105],
-            'STATE': [0.119496018, 0.089888389, 0.07827775, 0.072670547, 0.064865747, 0.062475027, 
-                      0.050854399, 0.049988679, 0.046685624, 0.043482459, 0.036553368, 0.033316907, 
-                      0.03284409, 0.031895128, 0.030473349, 0.029424496, 0.028928372, 0.020664074, 
-                      0.018809435, 0.015855997, 0.007232093, 0.006562825, 0.005510642, 0.005430729, 
-                      0.005330838, 0.00238739, 0.001664846, 0.001534988, 0.001481713, 0.001328547, 
-                      0.001288591, 0.001175382, 0.00107882, 0.000526091, 1.66485E-05]
+            'STATE': [0.119496018, 0.089888389, 0.07827775, 0.072670547, 0.064865747, 0.062475027, 0.050854399, 0.049988679,
+                      0.046685624, 0.043482459, 0.036553368, 0.033316907, 0.03284409, 0.031895128, 0.030473349, 0.029424496,
+                      0.028928372, 0.020664074, 0.018809435, 0.015855997, 0.007232093, 0.006562825, 0.005510642, 0.005430729,
+                      0.005330838, 0.00238739, 0.001664846, 0.001534988, 0.001481713, 0.001328547, 0.001288591, 0.001175382,
+                      0.00107882, 0.000526091, 1.66485E-05]
         }
 
         # List of flag variables
@@ -134,9 +149,7 @@ class DataGenerator:
             prob[flag_var] = [0.5, 0.5]
 
         # Categorical data
-        self.categorical_data = pd.DataFrame({
-            col: np.random.choice(cat_variables[col], self.n_samples, p=prob[col]) for col in cat_variables
-        })
+        self.categorical_data = pd.DataFrame({col: np.random.choice(cat_variables[col], self.n_samples, p=prob[col]) for col in cat_variables})
         self.categorical_data['ACCOUNT_NO'] = self.categorical_data.index
 
     def combine_data(self):
@@ -152,6 +165,7 @@ class DataGenerator:
     def export_to_csv(self):
         self.final_data.to_csv(self.export_path, index=False)
         print(f"Dataset exported to: {os.path.abspath(self.export_path)}")
+
 
 class DataPipeline:
     def __init__(self, data_generator):
